df610f2a53c08114ded0cbcbb267a354615ce6b6
Viorel Belfiore <belfiore.viorel@gmail.com>
2016-12-20 21:14:33 -0500

    made design changes

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -16,9 +16,9 @@ class AboutMe extends Component {
                   src="../style/img/profile_pic.jpg"
                   alt="profile pic"/>
                 <p className="aboutMe_p">
-                  I am a 27 year old web developer with lots of sales experience and
-                  an exceptional track record of meeting goals under time pressure with a
-                  focus on maximizing customer satisfaction.
+                  I'm an energetic web developer with many years of sales experience.
+                    I have an exceptional track record of meeting goals under time
+                    pressure while maximizing on customer satisfaction.
                 </p>
               </div>
               <div className="accomplishment_box">
@@ -36,17 +36,17 @@ class AboutMe extends Component {
             <div className="skillbox">
               <h2 id="skillTitle">Skills</h2>
               <ul className="skill_ul">
-                <li className="skill_li"><img className="skillImage" id="css3" src="../style/img/css3.png" alt="css3"/></li>
-                <li className="skill_li"><img className="skillImage" id="html5" src="../style/img/html5.png" alt="html5"/></li>
-                <li className="skill_li"><img className="skillImage" id="react" src="../style/img/react.png" alt="react"/></li>
-                <li className="skill_li"><img className="skillImage" id="javascript" src="../style/img/javascript.png" alt="javascript"/></li>
-                <li className="skill_li"><img className="skillImage" id="jquery" src="../style/img/jquery.gif" alt="jquery"/></li>
-                <li className="skill_li"><img className="skillImage" id="ruby" src="../style/img/ruby.png" alt="ruby"/></li>
-                <li className="skill_li"><img className="skillImage" id="rails" src="../style/img/rails.png" alt="rails"/></li>
-                <li className="skill_li"><img className="skillImage" id="nodejs" src="../style/img/nodejs.png" alt="nodejs"/></li>
-                <li className="skill_li"><img className="skillImage" id="express" src="../style/img/express.png" alt="express"/></li>
-                <li className="skill_li"><img className="skillImage" id="postgresql" src="../style/img/postgresql.png" alt="postgresql"/></li>
-                <li className="skill_li"><img className="skillImage" id="photoshop" src="../style/img/photoshop.png" alt="photoshop"/></li>
+                <li className="skill_li"><img className="skillImage" id="css3" src="../style/img/css3.png" alt="css3"/><div>CSS3</div></li>
+                <li className="skill_li"><img className="skillImage" id="html5" src="../style/img/html5.png" alt="html5"/><div>HTML5</div></li>
+                <li className="skill_li"><img className="skillImage" id="react" src="../style/img/react.png" alt="react"/><div>React</div></li>
+                <li className="skill_li"><img className="skillImage" id="javascript" src="../style/img/javascript.png" alt="javascript"/><div>Javascript</div></li>
+                <li className="skill_li"><img className="skillImage" id="jquery" src="../style/img/jquery.gif" alt="jquery"/><div>JQuery</div></li>
+                <li className="skill_li"><img className="skillImage" id="ruby" src="../style/img/ruby.png" alt="ruby"/><div>Ruby</div></li>
+                <li className="skill_li"><img className="skillImage" id="rails" src="../style/img/rails.png" alt="rails"/><div>Ruby on Rails</div></li>
+                <li className="skill_li"><img className="skillImage" id="nodejs" src="../style/img/nodejs.png" alt="nodejs"/><div>NodeJS</div></li>
+                <li className="skill_li"><img className="skillImage" id="postgresql" src="../style/img/postgresql.png" alt="postgresql"/><div>Postgresql</div></li>
+                <li className="skill_li"><img className="skillImage" id="express" src="../style/img/express.png" alt="express"/><div>Express</div></li>
+                <li className="skill_li"><img className="skillImage" id="photoshop" src="../style/img/photoshop.png" alt="photoshop"/><div>Photoshop</div></li>
               </ul>
             </div>
       </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -8,22 +8,22 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
-        <h3 id="projects_title">Projects</h3>
-        <div id="all_projects">
-          <div className="hover panel">
-            <div className="projects_panel front poliChange">
-              <h4 id="polichange_title">PoliChange</h4>
-              <div id="polichange_info">
-                <a id="polichange_image" href="https://polichange.herokuapp.com/">
-                  <div id="poliChangeLogoDiv">
-                    <h2 id="poliChangeLogo">P¢ </h2>
-                  </div>
-                </a>
-                <p id="polichange_p">
-                  This is an app that seeks to give users the ability
-                  to search for various politicians and get back data on who is
-                  donating to them.
-                </p>
+        <div className="projects_overlay">
+          <h3 id="projects_title">Projects</h3>
+          <div id="all_projects">
+            <div className="hover panel">
+              <div className="projects_panel front poliChange">
+                <h4 id="polichange_title">PoliChange</h4>
+                <div id="polichange_info">
+                  <a id="polichange_image_link" href="https://polichange.herokuapp.com/">
+                    <img id="poliChangeLogo" src="../style/img/PoliChangeLogo.jpg" alt="polichange" />
+                  </a>
+                  <p id="polichange_p">
+                    This is an app that seeks to give users the ability
+                    to search for various politicians and get back data on who is
+                    donating to them.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
